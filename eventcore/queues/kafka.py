a34cdcc44b26308043e1492ec1010791968c48e6
Tyler de Graaff <tylerdegraaff@gmail.com>
2018-07-09 20:50:38 +0200

    Update kafka.py with producer

--- eventcore/queues/kafka.py
+++ eventcore/queues/kafka.py
@@ -1,41 +1,32 @@
 import json
 
 from eventcore import Queue, Message
-
-from kafka import (KafkaConsumer,
-                   KafkaProducer)
+from confluent_kafka import Producer, Consumer
 
 
 class KafkaQueue(Queue):
-    def __init__(self,
-                 server):
-        self.kafka_consumer = KafkaConsumer(topic=self._topic,
-                                            group_id=self._group_id,
-                                            bootstrap_servers=[server])
-        self.kafka_producer = KafkaProducer(topic=self._topic,
-                                            group_id=self._group_id,
-                                            bootstrap_servers=[server])
+    def __init__(self, server, group_id=None):
+        self.kafka_producer = Producer({'bootstrap.servers': server})
+        if group_id:
+            self.kafka_consumer = Consumer({'bootstrap.servers': server,
+                                            'group.id': group_id})
 
     def read(self, topic=None):
-        # TODO: Remove confusing names within project (?)
-        for resource in self.kafka_consumer:
-            # message value and key are raw bytes -- decode if necessary!
-            # e.g., for unicode: `message.value.decode('utf-8')`
-            message_value = json.loads(resource.value.decode('utf-8'))
-            decoded_resource_key = resource.key.decode('utf-8')
-
-            message = self.prepare(topic=None,
-                                   event=message_value.get('event'),
-                                   subject=message_value.get('subject'),
-                                   data=message_value.get('data'))
-
-            message.message_id = decoded_resource_key
-            yield message
-
-    def enqueue(self, message):
+        self.kafka_consumer.subscribe(topic)
         pass
 
+    def enqueue(self, message):
+        message_body = {
+            'event': message.event,
+            'data': message.data
+        }
+        self.kafka_producer.produce(message.topic,
+                                    key=message.subject,
+                                    value=json.dumps(message_body))
+        self.kafka_producer.flush()
+
     def dequeue(self, message):
+        """ Not available in Kafka """
         pass
 
     def prepare(self, topic, event, subject, data):
