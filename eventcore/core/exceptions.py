56aa3902c726f85ec29455b7f1fec45c297ad3ba
Maikel <maikelboogerd@gmail.com>
2018-07-11 22:26:38 +0200

    Add some exceptions

--- eventcore/core/exceptions.py
+++ eventcore/core/exceptions.py
@@ -1,5 +1,19 @@
-class NoProducerError(Exception):
+class ProducerError(Exception):
+    """
+    Exception is thrown when producing fails.
+    """
+    pass
+
+
+class NoProducerError(ProducerError):
     """
     Exception is thrown when trying to dispatch without a producer.
+    """
+    pass
+
+
+class ConsumerError(Exception):
+    """
+    Exception is thrown when consuming fails.
     """
     pass
