35dbe57a7007c0554ddd78994a700199f08f8a9c
Maikel <maikelboogerd@gmail.com>
2018-09-30 21:06:39 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='eventcore',
-    version='0.3.2',
+    version='0.3.3',
     description='Produce and consume events with any queue.',
     author='Maikel van den Boogerd',
     author_email='maikelboogerd@gmail.com',
