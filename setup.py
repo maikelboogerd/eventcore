a968af256fa86af48af62250e4acb25f412ac988
Maikel <maikelboogerd@gmail.com>
2019-05-26 00:39:56 +0200

    Correct version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ tests_require = [
 
 setuptools.setup(
     name='eventcore',
-    version='0.4.1',
+    version='0.4.0',
     description='Produce and consume events with any queue.',
     author='Maikel van den Boogerd',
     author_email='maikelboogerd@gmail.com',
