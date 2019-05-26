ad99f94385fa7feb313bdf6bda119cc0419d748b
Maikel <maikelboogerd@gmail.com>
2019-05-26 13:33:45 +0200

    Add tests for event construct

--- tests/integration/test_constructors.py
+++ tests/integration/test_constructors.py
@@ -2,12 +2,25 @@ import unittest
 
 from mockito import mock
 
+from eventcore.event import Event
 from eventcore.dummy import DummyConsumer, DummyProducer
 
 
 class TestConstructors(unittest.TestCase):
     """
     """
+
+    def test_event_constructor(self):
+        """
+        Check if the `Event` subclass can be initialized.
+        """
+        class SomeEvent(Event):
+            topic = 'some-topic'
+
+        try:
+            SomeEvent('a-subject', {'name': 'a-name'})
+        except:
+            self.fail('Cannot initialize `Event` subclass with known params')
 
     def test_dummy_consumer_constructor(self):
         """
